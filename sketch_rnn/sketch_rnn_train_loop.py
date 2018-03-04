22d3b1b32a7ed4486f0810248794e9adc3f6cd1b
mhyz1n4 <mhyz1n4@gmail.com>
2018-03-03 22:24:40 -0600

    new update

--- sketch_rnn/sketch_rnn_train_loop.py
+++ sketch_rnn/sketch_rnn_train_loop.py
@@ -30,9 +30,17 @@ import requests
 import six
 from six.moves import cStringIO as StringIO
 import tensorflow as tf
+import uuid
 
 from magenta.models.sketch_rnn import model as sketch_rnn_model
 from magenta.models.sketch_rnn import utils
+
+from magenta.models.sketch_rnn.sketch_rnn_train import *
+from magenta.models.sketch_rnn.model import *
+from magenta.models.sketch_rnn.utils import *
+from magenta.models.sketch_rnn.rnn import *
+
+
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -442,11 +450,6 @@ def trainer(model_params, datasets):
   """Train a sketch-rnn model."""
 
   train_set = datasets[0]
-
-  #test
-  #print(train_set.strokes[0])
-
-
   valid_set = datasets[1]
   test_set = datasets[2]
   model_params = datasets[3]
@@ -478,11 +481,217 @@ def trainer(model_params, datasets):
   lstm_W_xh_ = [v for v in tf.trainable_variables() if v.name == "vector_rnn/RNN/LSTMCell/W_xh:0"][0].eval()
   lstm_W_hh_ = [v for v in tf.trainable_variables() if v.name == "vector_rnn/RNN/LSTMCell/W_hh:0"][0].eval()
   lstm_bias_ = [v for v in tf.trainable_variables() if v.name == "vector_rnn/RNN/LSTMCell/bias:0"][0].eval()
-  print(output_w_)
-  print(output_b_)
-  print(lstm_W_xh_)
-  print(lstm_W_hh_)
-  print(lstm_bias_)
+  print(output_w_.shape())
+  print(output_b_.shape())
+  print(lstm_W_xh_.shape())
+  print(lstm_W_hh_.shape())
+  print(lstm_bias_.shape())
+
+  dec_output_w = output_w_;
+  dec_output_b = output_b_;
+  dec_lstm_W_xh = lstm_W_xh_;
+  dec_lstm_W_hh = lstm_W_hh_;
+  dec_lstm_bias = lstm_bias_;
+  dec_num_units = dec_lstm_W_hh.shape[0];
+  dec_input_size = dec_lstm_W_xh.shape[0];
+  dec_lstm = SketchLSTMCell(dec_num_units, dec_input_size, dec_lstm_W_xh, dec_lstm_W_hh, dec_lstm_bias)
+
+  result = generate()
+  output = []
+  entry = []
+  for i in result:
+      print(i)
+      entry.extend(i[0], i[1])
+      break;
+  print(entry)
+
+
+class SketchLSTMCell(object):
+
+    def __init__(self, num_units, input_size, Wxh, Whh, bias):
+        self.num_units = num_units;
+        self.input_size = input_size;
+        self.Wxh = Wxh;
+        self.Whh = Whh;
+        self.bias = bias;
+        self.forget_bias = 1.0;
+        self.Wfull=np.transpose(np.concatenate((np.transpose(Wxh), np.transpose(Whh)),axis=1))
+
+    def get_pdf(self,s):
+        h = s[0];
+        NOUT = N_mixture;
+        z = np.dot(h, dec_output_w) + dec_output_b;
+        z_pen_logits = z[0:3];
+        z_pi = z[3+NOUT*0:3+NOUT*1];
+        z_mu1 = z[3+NOUT*1:3+NOUT*2];
+        z_mu2 = z[3+NOUT*2:3+NOUT*3];
+        z_sigma1 = np.exp(z[3+NOUT*3:3+NOUT*4]);
+        z_sigma2 = np.exp(z[3+NOUT*4:3+NOUT*5]);
+        z_corr = np.tanh(z[3+NOUT*5:3+NOUT*6]);
+        z_pen = softmax(z_pen_logits)
+        z_pi = softmax(z_pi);
+        return [z_pi, z_mu1, z_mu2, z_sigma1, z_sigma2, z_corr, z_pen];
+
+    def zero_state(self):
+        return [np.zeros(self.num_units), np.zeros(self.num_units)]
+
+    def __call__(self,x,h,c):
+        concat = np.concatenate((x, h));
+        d = np.dot(concat, self.Wfull)
+        hidden = d + self.bias
+        num_units = self.num_units;
+        forget_bias = self.forget_bias;
+        i = expit(hidden[0*num_units:1*num_units]);
+        g = np.tanh(hidden[1*num_units:2*num_units]);
+        f = expit(hidden[2*num_units:3*num_units] + forget_bias);
+        o = expit(hidden[3*num_units:4*num_units]);
+
+        new_c = c*f + g*i
+        new_h = np.tanh(new_c) * o;
+        return [new_h, new_c];
+
+
+N_mixture = 20
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+def sample(z, temperature=None, softmax_temperature=None):
+    temp=0.25;
+    if temperature is not None:
+        temp = temperature;
+    softmax_temp = 0.5+temp*0.5;
+    if softmax_temperature is not None:
+        softmax_temp = softmax_temperature;
+    z_0 = adjust_temp(z[0], softmax_temp);
+    z_6 = adjust_temp(z[6], softmax_temp);
+    idx = sample_softmax(z_0);
+    mu1 = z[1][idx];
+    mu2 = z[2][idx];
+    sigma1 = z[3][idx]*np.sqrt(temp);
+    sigma2 = z[4][idx]*np.sqrt(temp);
+    corr = z[5][idx];
+    pen_idx = sample_softmax(z_6);
+    penstate = [0, 0, 0];
+    penstate[pen_idx] = 1;
+    delta = birandn(mu1, mu2, sigma1, sigma2, corr);
+    return [delta[0]*scale_factor, delta[1]*scale_factor, penstate[0], penstate[1], penstate[2]];
+
+def adjust_temp(z_old, temp):
+    z = z_old.copy();
+    z = np.log(z)/temp
+    x = z.max()
+    z = z - x
+    z = np.exp(z)
+    x = z.sum()
+    z = z/x
+    return z;
+
+def randf(a, b):
+    return np.random.random()*(b-a)+a
+
+def sample_softmax(z_sample):
+    x = randf(0,1)
+    N = z_sample.shape[0]
+    accumulate = 0
+    for i in range(N):
+        accumulate += z_sample[i];
+        if accumulate >= x:
+            return i
+    return -1;
+return_v = False;
+v_val = 0.0;
+
+def gaussRandom():
+    global return_v, v_val
+    if return_v:
+        return_v = False;
+        return v_val;
+
+    u = 2*np.random.random()-1;
+    v = 2*np.random.random()-1;
+    r = u*u + v*v;
+    if r == 0 or  r > 1:
+        return gaussRandom();
+    c = np.sqrt(-2*np.log(r)/r);
+    v_val = v*c;
+    return_v = True;
+    return u*c;
+
+def randn(mu, std):
+    return mu+gaussRandom()*std
+
+def birandn(mu1, mu2, std1, std2, rho):
+    z1 = randn(0,1);
+    z2 = randn(0,1);
+    x = np.sqrt(1-rho*rho)*std1*z1 + rho*std1*z2 + mu1;
+    y = std2*z2 + mu2;
+    return [x, y];
+
+def generate(temperature = None, softmax_temperature = None):
+    temp=0.25;
+    if temperature is not None:
+        temp = temperature;
+    softmax_temp = 0.5+temp*0.5;
+    if softmax_temperature is not None:
+        softmax_temp = softmax_temperature
+    init_state = dec_lstm.zero_state()
+    h = init_state[0];
+    c = init_state[1];
+
+    x = np.array([0, 0, 0, 0, 0],dtype=np.float)
+    result = [];
+    max_seq_len = 125
+    for i in range(max_seq_len):
+        lstm_input = x;
+        rnn_state = dec_lstm(lstm_input, h, c);
+        pdf = dec_lstm.get_pdf(rnn_state)
+        [dx, dy, pen_down, pen_up, pen_end] = sample(pdf, temp, softmax_temp);
+        result.append([dx, dy, pen_down, pen_up, pen_end]);
+        if pen_end == 1:
+            return result;
+        x = np.array([dx/scale_factor, dy/scale_factor, pen_down, pen_up, pen_end]);
+
+        h = rnn_state[0];
+        c = rnn_state[1];
+    result.append([0, 0, 0, 0, 1]);
+    return result
+max_seq_len = 123
+scale_factor = 99.698
+
+def get_sketch(sketch):
+    #sketch = generate()
+    #print(sketch)
+    xsum, ysum = [0],[0]
+    for i,l in enumerate(sketch):
+        xsum.append(l[0]+xsum[i])
+        ysum.append(l[1]+ysum[i])
+    width = np.max(xsum) - np.min(xsum)
+    height = np.max(ysum) - np.min(ysum)
+    drawing = np.zeros((int(height)+40,int(width)+40,3),dtype=np.uint8)
+    drawing[:,:] = (255,255,255)
+    x_start, y_start = 20-int(np.min(xsum)),20-int(np.min(ysum))
+
+    x, y = x_start,y_start
+    prev_pen = [1, 0, 0]
+
+    x_lines = []
+    y_lines = []
+    for l in sketch:
+        dx, dy, pen_down, pen_up, pen_end = l
+        dx = int(dx)
+        dy = int(dy)
+        if prev_pen[2] == 1:
+            break
+        if prev_pen[0] == 1:
+            x_lines.extend([x,x+dx])
+            y_lines.extend([y,y+dy])
+            #lines.append([[x,y],[x+dx,y+dy]])
+        x += dx;
+        y += dy;
+        prev_pen = [pen_down, pen_up, pen_end]
+    return [x_lines,y_lines]
 
 
 
@@ -521,7 +730,7 @@ def main(unused_argv):
       #print(datasets[0].strokes[j])
 
 
-  retrain_times = 3
+  retrain_times = 1
   for i in range(retrain_times):
 
       trainer(model_params, datasets)
